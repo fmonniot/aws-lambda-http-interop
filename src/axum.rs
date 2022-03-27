68c73aef1c68b8760ff91dcd9d524f0f6ed17979
François Monniot <francoismonniot@gmail.com>
2022-03-27 13:15:18 -0700

    Axum interop now correctly map the response body

--- src/axum.rs
+++ src/axum.rs
@@ -34,33 +34,66 @@ impl tower::Service<lambda_http::Request> for AxumService {
             lambda_http::Body::Text(s) => s.into(),
             lambda_http::Body::Binary(b) => b.into(),
         });
-        let fut = Box::pin(self.router.call(r));
+        let fut = Box::new(self.router.call(r));
 
-        TransformResponse { fut }
+        TransformResponse::WaitResponse { fut }
     }
 }
 
-/// Future that will convert the hyper response body to the lambda_http's body
-///
-/// This is used by the `AxumService` wrapper and is completely internal to the `service` function.
-#[doc(hidden)]
-pub struct TransformResponse {
-    fut: Pin<Box<RouterFuture<hyper::Body>>>,
+// To get autocompletion with rust-analyzer, set the option
+// rust-analyzer.experimental.procAttrMacros to true
+pin_project_lite::pin_project! {
+    /// Future that will convert the hyper response body to the lambda_http's body
+    ///
+    /// This is used by the `AxumService` wrapper and is completely internal to the `service` function.
+    #[project = TransformProj]
+    #[doc(hidden)]
+    pub enum TransformResponse {
+        WaitResponse{ #[pin] fut: Box<RouterFuture<hyper::Body>> },
+        WaitBody {
+            parts: Option<http::response::Parts>,
+            body: Pin<Box<dyn Future<Output = Result<axum::body::Bytes, axum::Error>>>>
+        }
+    }
 }
 
 impl Future for TransformResponse {
     type Output = Result<lambda_http::Response<lambda_http::Body>, lambda_http::Error>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        match self.fut.as_mut().poll(cx) {
-            Poll::Pending => Poll::Pending,
-            Poll::Ready(result) => {
-                let r = result.unwrap();
-                let r = r.map(|_body| {
-                    todo!("How do I accumulate the body here ?") // TODO
-                });
+        match self.as_mut().project() {
+            TransformProj::WaitResponse { fut } => {
+                match fut.poll(cx) {
+                    Poll::Pending => Poll::Pending,
+                    Poll::Ready(result) => {
+                        let response = result.unwrap(); // TODO
 
-                Poll::Ready(Ok(r))
+                        let (parts, body) = response.into_parts();
+                        let parts = Some(parts);
+                        let body = Box::pin(hyper::body::to_bytes(body));
+
+                        // We got the response, switching to next polling phase: getting the body
+                        self.set(TransformResponse::WaitBody { parts, body });
+
+                        // TODO Do we need to wake up the waker ?
+                        Poll::Pending
+                    }
+                }
+            }
+            TransformProj::WaitBody { parts, body } => {
+                match body.as_mut().poll(cx) {
+                    Poll::Pending => Poll::Pending,
+                    Poll::Ready(body) => {
+                        let bytes = body.unwrap(); // TODO
+                        let body = lambda_http::Body::Binary(bytes.to_vec());
+
+                        let parts = parts.take().expect("parts cannot be None");
+
+                        let response = lambda_http::Response::from_parts(parts, body);
+
+                        Poll::Ready(Ok(response))
+                    }
+                }
             }
         }
     }
