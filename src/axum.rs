b64befe89f5aaa836d46418fe01c9104aee9c4f0
François Monniot <francoismonniot@gmail.com>
2022-03-28 08:07:56 -0700

    Test (and fix) axum::TransformResponse

--- src/axum.rs
+++ src/axum.rs
@@ -76,6 +76,7 @@ impl Future for TransformResponse {
                         self.set(TransformResponse::WaitBody { parts, body });
 
                         // TODO Do we need to wake up the waker ?
+                        cx.waker().wake_by_ref();
                         Poll::Pending
                     }
                 }
@@ -85,7 +86,12 @@ impl Future for TransformResponse {
                     Poll::Pending => Poll::Pending,
                     Poll::Ready(body) => {
                         let bytes = body.unwrap(); // TODO
-                        let body = lambda_http::Body::Binary(bytes.to_vec());
+
+                        let body = if bytes.is_empty() {
+                            lambda_http::Body::Empty
+                        } else {
+                            lambda_http::Body::Binary(bytes.to_vec())
+                        };
 
                         let parts = parts.take().expect("parts cannot be None");
 
@@ -96,5 +102,74 @@ impl Future for TransformResponse {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tower::Service;
+
+    // Utility function to test out the TransformResponse implementation.
+    // Because the struct takes a RouterFuture as a parameter, and because
+    // that struct isn't intented to be created outside of axum, we resort
+    // to creating a dummy Router which will create such future for us.
+    async fn transform_response_test<F, Fut, Res>(axum_body: F, lambda_body: lambda_http::Body)
+    where
+        F: FnOnce() -> Fut + Clone + Send + 'static,
+        Fut: std::future::Future<Output = Res> + Send,
+        Res: axum::response::IntoResponse,
+    {
+        let mut app = axum::Router::new().route("/", axum::routing::get(axum_body));
+
+        let request = http::Request::get("https://www.rust-lang.org/")
+            .body(hyper::Body::empty())
+            .unwrap();
+
+        let fut = Box::new(app.call(request));
+
+        let transform = super::TransformResponse::WaitResponse { fut };
+
+        let res = transform.await;
+
+        match res {
+            Ok(res) => assert_eq!(res.into_body(), lambda_body),
+            Err(e) => panic!("transform future resulted in an error: {:?}", e),
+        }
+    }
+
+    #[tokio::test]
+    async fn transform_empty_response() {
+        transform_response_test(|| async { () }, lambda_http::Body::Empty).await;
+    }
+
+    #[tokio::test]
+    async fn transform_one_chunk_response() {
+        let bytes = "hello".as_bytes().to_owned();
+        let b2 = bytes.clone();
+        transform_response_test(move || async { b2 }, lambda_http::Body::Binary(bytes)).await;
+    }
+
+    #[tokio::test]
+    async fn transform_multiple_chunks_response() {
+        let chunks = vec!["Hello", " ", "World!"];
+        let stream_chunks = chunks.clone();
+
+        let bytes: Vec<u8> = chunks
+            .into_iter()
+            .flat_map(|s| s.to_string().into_bytes())
+            .collect();
+
+        transform_response_test(
+            move || async {
+                let stream_chunks: Vec<Result<_, std::io::Error>> = stream_chunks
+                    .into_iter()
+                    .map(|s| Ok(s.to_string()))
+                    .collect();
+                let stream = futures::stream::iter(stream_chunks);
+                axum::body::StreamBody::new(stream)
+            },
+            lambda_http::Body::Binary(bytes),
+        )
+        .await;
     }
 }
